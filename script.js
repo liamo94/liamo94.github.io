17c46193a792432040d244f477ab3cc1b6e78cf3
Liam O'Donnell <liam094@hotmail.com>
2019-11-24 21:05:31 +0000

    added links to sites

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log('opened');
+console.log(`Liam O'Donnell`);
 
 
 // document.getElementById ("myButton").addEventListener ("click", myFunction, false);
@@ -6,3 +6,41 @@ console.log('opened');
 function myFunction() {
   alert("Hello! I am an alert box!!");
 }
+
+
+var LoadImage = (() => {
+
+  var showImage = function () {
+    console.log('called again');
+    var images = document.querySelectorAll('img[data-src]');
+    console.log(images);
+    images.forEach(img => {
+      img.setAttribute('src', img.getAttribute('data-src'));
+      img.onload = function () {
+        img.removeAttribute('data-src');
+      };
+    });
+  };
+
+  return {
+    showImage: showImage
+  }
+})();
+
+var checkImageLoc = function () {
+  if (document.querySelectorAll('img')[0].getBoundingClientRect().y < screen.height) {
+    // console.log(document.querySelectorAll('img')[0].getBoundingClientRect().y);
+    console.log('he div is there');
+    LoadImage.showImage();
+    document.removeEventListener('scroll', checkImageLoc, true);
+  }
+};
+
+function init() {
+  console.log('called');
+  // document.addEventListener('scroll', checkImageLoc, true);
+  LoadImage.showImage();
+};
+
+LoadImage.showImage();
+
